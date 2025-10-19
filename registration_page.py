e0bdc772e6ebc9cee75f0c1085d5950b2a0aea71
Goncharov Anton <goncharrov.a@icloud.com>
2025-10-19 22:12:58 +0300

    refactor: add class BasePage, User, RegistrationPage

---
+++ registration_page.py
@@ -0,0 +1,84 @@
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+
+class BasePage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, 5)
+
+    def find_element(self, selector):
+        return self.wait.until(EC.presence_of_element_located(selector))
+
+    def not_find_element(self, selector):
+        return self.wait.until_not(EC.presence_of_element_located(selector))
+
+    def click(self, selector, force=False):
+        element = self.wait.until(EC.element_to_be_clickable(selector))
+        if force:
+            element.click()
+        else:
+            self.driver.execute_script("arguments[0].click();", element)
+
+    def fill(self, selector, value):
+        element = self.find_element(selector)
+        element.send_keys(value)
+
+
+class User:
+    def __init__(self, unique):
+        self.first_name = "Ivan"
+        self.last_name = "Petrov"
+        self.email = f"test{unique}{unique}@mail.ru"
+        self.phone_number = "+79993338811"
+        self.password = "random_pass"
+
+
+class RegistrationPage(BasePage):
+    FIRST_NAME = (By.ID, "input-firstname")
+    LAST_NAME = (By.ID, "input-lastname")
+    EMAIL = (By.ID, "input-email")
+    PHONE = (By.ID, "input-telephone")
+    PASSWORD = (By.ID, "input-password")
+    CONFIRM_PASSWORD = (By.ID, "input-confirm")
+    POLICY_AGREEMENT = (By.ID, "input-agree")
+    CONTINUE_BUTTON = (By.CSS_SELECTOR, '[value="Continue"]')
+    CONTENT = (By.ID, "content")
+    TEXT_ASSERT = (By.XPATH, '//*[text() = " Your Account Has Been Created!"]')
+
+    def check_form_is_visible(self):
+        assert self.find_element(self.FIRST_NAME)
+        assert self.find_element(self.LAST_NAME)
+        assert self.find_element(self.EMAIL)
+        assert self.find_element(self.PHONE)
+        assert self.find_element(self.PASSWORD)
+        assert self.find_element(self.CONFIRM_PASSWORD)
+        assert self.find_element(self.POLICY_AGREEMENT)
+        assert self.find_element(self.CONTINUE_BUTTON)
+
+    def check_form_is_not_visible(self):
+        assert self.not_find_element(self.FIRST_NAME)
+        assert self.not_find_element(self.LAST_NAME)
+        assert self.not_find_element(self.EMAIL)
+        assert self.not_find_element(self.PHONE)
+        assert self.not_find_element(self.PASSWORD)
+        assert self.not_find_element(self.CONFIRM_PASSWORD)
+        assert self.not_find_element(self.POLICY_AGREEMENT)
+        assert self.not_find_element(self.CONTINUE_BUTTON)
+
+    def fill_registration_form(self, user: User):
+        self.fill(self.FIRST_NAME, user.first_name)
+        self.fill(self.LAST_NAME, user.last_name)
+        self.fill(self.EMAIL, user.email)
+        self.fill(self.PHONE, user.phone_number)
+        self.fill(self.PASSWORD, user.password)
+        self.fill(self.CONFIRM_PASSWORD, user.password)
+        self.click(self.POLICY_AGREEMENT, force=True)
+        self.click(self.CONTINUE_BUTTON)
+
+    def check_that_success_form_is_visible(self):
+        assert self.find_element(self.CONTENT)
+        assert self.find_element(self.TEXT_ASSERT)
+
+
