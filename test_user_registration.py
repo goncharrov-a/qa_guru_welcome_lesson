760578869efb168476144b4e7e0483de232cedfb
Goncharov Anton <goncharrov.a@icloud.com>
2025-10-20 09:29:44 +0300

    test: add blank form negative test

--- test_user_registration.py
+++ test_user_registration.py
@@ -1,6 +1,7 @@
 import os
 import random
 import time
+from os import register_at_fork
 
 import pytest
 from dotenv import load_dotenv
@@ -39,3 +40,8 @@ def test_user_registration_pom(registration_page, unique_user):
     registration_page.fill_registration_form(unique_user)
     registration_page.check_form_is_not_visible()
     registration_page.check_that_success_form_is_visible()
+
+
+def test_blank_form_warnings(registration_page):
+    registration_page.check_form_is_visible()
+    registration_page.click_continue_button()
